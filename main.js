ac8fea224909b98fb8ee86ccc1d9fd70d29d4a4a
Pedro-723 <84990702+Pedro-723@users.noreply.github.com>
2021-06-16 20:18:04 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -1,35 +1,22 @@
-function game() {
+let moves = ["rock", "paper", "scissor"];
 
-    let moves = ["rock", "paper", "scissor"];
+function computerPlay() {
+    return moves[Math.floor(Math.random() * 3)];
+}
 
-    function computerPlay() {
-        return moves[Math.floor(Math.random() * 3)];
+
+function playRound(playerSelection, computerSelection) {
+
+
+    if (playerSelection === 'rock' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'scissor' ||
+        playerSelection === 'scissor' && computerSelection === 'rock') {
+        return alert('You lose!')
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissor' && computerSelection === 'paper') {
+        return alert('You win!')
+    } else if (playerSelection === computerSelection) {
+        return alert("Draw!")
     }
-
-    let computerChoice = computerPlay();
-
-    let playerChoice = window.prompt("Pick your move").toLowerCase();
-
-
-
-    function playRound(playerSelection, computerSelection) {
-
-
-
-        if (playerSelection === computerSelection) {
-            return alert("Draw!")
-        } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
-            playerSelection === 'paper' && computerSelection === 'scissor' ||
-            playerSelection === 'scissor' && computerSelection === 'rock') {
-            return alert('You lose!')
-        } else if (playerSelection === 'rock' && computerSelection === 'scissor' ||
-            playerSelection === 'paper' && computerSelection === 'rock' ||
-            playerSelection === 'scissor' && computerSelection === 'paper') {
-            return alert('You win!')
-        } else {
-            return alert("Invalid input");
-        }
-    }
-
-    playRound(playerChoice, computerChoice);
 }
